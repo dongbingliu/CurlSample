940ce22c61dbcd84a2ddcb8509224365b0989637
dongbingliu <dongbingliu@tencent.com>
2021-05-01 21:39:19 +0800

    支持 mac 编译运行

--- cmake-build-debug/CMakeFiles/cprSample.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/cprSample.dir/DependInfo.cmake
@@ -4,30 +4,25 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "H:/Work/ClionProject/cprSample/main.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/main.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/auth.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/auth.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/bearer.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/bearer.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/cookies.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/cookies.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/cprtypes.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/cprtypes.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/curl_container.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/curl_container.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/curlholder.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/curlholder.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/error.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/error.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/multipart.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/multipart.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/parameters.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/parameters.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/payload.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/payload.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/proxies.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/proxies.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/response.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/response.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/session.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/session.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/timeout.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/timeout.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/unix_socket.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/unix_socket.cpp.obj"
-  "H:/Work/ClionProject/cprSample/src/cpr/util.cpp" "H:/Work/ClionProject/cprSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/util.cpp.obj"
+  "/Users/dongbingliu/Github/CurlSample/main.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/main.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/auth.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/auth.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/bearer.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/bearer.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/cookies.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/cookies.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/cprtypes.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/cprtypes.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/curl_container.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/curl_container.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/curlholder.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/curlholder.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/error.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/error.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/multipart.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/multipart.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/parameters.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/parameters.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/payload.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/payload.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/proxies.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/proxies.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/response.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/response.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/session.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/session.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/timeout.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/timeout.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/unix_socket.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/unix_socket.cpp.o"
+  "/Users/dongbingliu/Github/CurlSample/src/cpr/util.cpp" "/Users/dongbingliu/Github/CurlSample/cmake-build-debug/CMakeFiles/cprSample.dir/src/cpr/util.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "MSVC")
-
-# Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_CXX
-  "CURL_STATICLIB"
-  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
